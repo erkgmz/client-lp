e48a54f0a92c7226ce9450bb1b9b64e0fc2a7b38
Erik Gomez <egoxiv@gmail.com>
2017-02-02 14:50:12 -0800

    Removes dist/

--- src/components/navigation/hamburgerStyles.js
+++ src/components/navigation/hamburgerStyles.js
@@ -1,4 +1,5 @@
 const hamburgerStyle = {
+
   bmBurgerButton: {
     position: 'fixed',
     width: '24px',
@@ -6,31 +7,38 @@ const hamburgerStyle = {
     right: '2rem',
     top: '1.7rem'
   },
+
   bmBurgerBars: {
     background: '#64FFDA'
   },
+
   bmCrossButton: {
     height: '24px',
     width: '24px',
     right: '2rem'
   },
+
   bmCross: {
     marginRight: '2rem',
     height: '24px',
     background: 'rgba(0, 0, 0, .8)'
   },
+
   bmMenu: {
     background: '#64FFDA',
     padding: '2.5em 1.5em 0',
     fontSize: '1.15em'
   },
+
   bmMorphShape: {
     fill: '#373a47'
   },
+
   bmItemList: {
     color: '#b8b7ad',
     padding: '0.8em'
   },
+
   bmOverlay: {
     background: 'rgba(0, 0, 0, 0.6)',
     zIndex: '1',
