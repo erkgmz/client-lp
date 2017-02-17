f8609fa5509dbe02bcf0d52213c79d5f761b1b5c
Erik Gomez <egoxiv@gmail.com>
2017-02-16 16:27:55 -0800

    Adds favicon.ico

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+require('./favicon.ico');
 
 let root = document.getElementById('app');
 render(<Router history={browserHistory} routes={routes} />, root);
