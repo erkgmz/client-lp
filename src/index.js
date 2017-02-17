24d97e6f854ed2dd97cc31d874828d245dc4cce3
Erik Gomez <egoxiv@gmail.com>
2017-02-16 18:42:17 -0800

    Adds social meta tags. Fixes deploy script

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 require('./favicon.ico');
+require('./assets/kailey_social.png');
 
 let root = document.getElementById('app');
 render(<Router history={browserHistory} routes={routes} />, root);
